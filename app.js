cfa9c3dba9c0a9c636e013fae00affb938074063
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-03-25 09:56:37 +0530

    Section-3-Chapter-33 Routing Requests

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const http = require('http');
 const server = http.createServer((req,res)=> {
     const url = req.url;
     if(url === '/') {
-        res.setHeader('Content-Type','text/html');
         res.write('<html');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<form action="message" method="POST"><input type="type" name="message"><button type="submit">Send</button></form>');
         res.write('</html>');
         return res.end();
     }
+    
+    res.setHeader('Content-Type','text/html');
+    res.write('<html');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<div>Hello from NodeJS</div>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(3000);
