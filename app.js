f85fc7f69d5dd9d5692438971ef622c126fbfa80
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-03-25 09:47:48 +0530

    Section-3-Chapter-31 Sending Responses

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const http = require('http');
 
 const server = http.createServer((req,res)=> {
     console.log(req.url,req.method, req.headers);
-    //process.exit(); // exited the event loop 
+    //process.exit(); // exited the event loop
+    res.setHeader('Content-Type','text/html');
+    res.write('<html');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<div>Hello from NodeJS</div>');
+    res.write('</html>');
+    res.end();
 });
 server.listen(3000);
