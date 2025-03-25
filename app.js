0c2d7e3a74609bd93bfc75e7ec7dee553b603c66
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-03-25 11:57:50 +0530

    Section-3-Chapter-39 Using the Node Modules System

--- app.js
+++ app.js
@@ -1,39 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req,res)=> {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/') {
-        res.write('<html');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<form action="message" method="POST"><input type="type" name="message"><button type="submit">Send</button></form>');
-        res.write('</html>');
-        return res.end();
-    }
-    
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', ()=> {
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            const message = parseBody.split('=')[1];
-            fs.writeFile('message.txt',message, err=> {
-                res.statusCode = 302;
-                res.setHeader('Loaction', '/');
-                return res.end();
-            });
-        })
-    }
-    res.setHeader('Content-Type','text/html');
-    res.write('<html');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<div>Hello from NodeJS</div>');
-    res.write('</html>');
-    res.end();
-});
+const routes = require('./routes');
+
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 server.listen(3000);
