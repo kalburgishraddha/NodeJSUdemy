f5af275623ec1bd679361306b0de92854b95392b
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-03-25 09:54:16 +0530

    Section-3-Chapter-33 Routing Requests

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=> {
-    console.log(req.url,req.method, req.headers);
-    //process.exit(); // exited the event loop
-    res.setHeader('Content-Type','text/html');
-    res.write('<html');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<div>Hello from NodeJS</div>');
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+    if(url === '/') {
+        res.setHeader('Content-Type','text/html');
+        res.write('<html');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<form action="message" method="POST"><input type="type" name="message"><button type="submit">Send</button></form>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 server.listen(3000);
