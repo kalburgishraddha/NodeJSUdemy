1a3f37e21037276c3b934bfd5080d1a6512791b1
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-03-25 09:42:48 +0530

    Section-3-Chapter-30 Understanding Requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=> {
-    console.log(req);
+    console.log(req.url,req.method, req.headers);
+    //process.exit(); // exited the event loop 
 });
 server.listen(3000);
