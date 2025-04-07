fbb5e4e1a2bbc68ff7322c4901b84a863d75fef6
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-04-07 11:56:41 +0530

    Section-5-Chapter-65 Parsing Incoming Requests

--- 01-adding-middleware/app.js
+++ 01-adding-middleware/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}))
 app.use('/add-product',(req,res,next)=> {
     console.log("inside another middleware");
-    res.send('<h1>The "Add product" Page</h1>');
+    res.send('<html><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button"><form></html>');
+});
+
+app.use('/product',(req,res,next)=> {
+    console.log("Product",req.body);
+    res.redirect('/');
 });
 
 app.use('/',(req,res,next)=> {
