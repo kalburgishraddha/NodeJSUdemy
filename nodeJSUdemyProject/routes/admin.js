d24d414da4fc10ceaabe545874bfae887323b7cb
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-04-09 10:59:09 +0530

    Section-5-Chapter-74 Using Helper Function for Navigation

--- nodeJSUdemyProject/routes/admin.js
+++ nodeJSUdemyProject/routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+ 
 const router = express.Router();
 
 router.get('/add-product',(req,res,next)=> {
     console.log("inside another middleware");
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 router.post('/product',(req,res,next)=> {
--- nodeJSUdemyProject/routes/shop.js
+++ nodeJSUdemyProject/routes/shop.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const router = express.Router();
 
 router.get('/',(req,res,next)=> {
     console.log("inside another middleware");
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
