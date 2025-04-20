60b2d9cbb9af631fb293c4e0c0da4fcf658ca74b
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-04-20 16:28:14 +0530

    Section-6-Chapter-92 Working with EJS

--- nodeJSUdemyProject/app.js
+++ nodeJSUdemyProject/app.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
-app.engine('hbs',expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname:'hbs'}))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
