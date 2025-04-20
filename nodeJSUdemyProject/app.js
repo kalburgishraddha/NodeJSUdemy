5a53fb8f54e4e66dc9b2462e5ca3120f3ac1f891
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-04-20 11:21:00 +0530

    Section-6-Chapter-89 Working with handlebars, Chapter-90 Converting our project to Handlebars

--- nodeJSUdemyProject/app.js
+++ nodeJSUdemyProject/app.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
-app.set('view engine', 'pug')
+app.engine('hbs',expressHbs())
+app.set('view engine', 'hbs')
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
--- nodeJSUdemyProject/routes/shop.js
+++ nodeJSUdemyProject/routes/shop.js
@@ -11,7 +11,12 @@ router.get('/',(req,res,next)=> {
     console.log("inside another middleware");
     console.log("shop.js products",adminData.products)
     //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop',{'prods':adminData.products, 'pageTitle':'Shop', 'path':'/'});
+    res.render('shop',{
+        prods:adminData.products,
+        pageTitle:'Shop',
+        path:'/', 
+        hasProducts: adminData.products.length>0
+    });
 });
 
 module.exports = router;
