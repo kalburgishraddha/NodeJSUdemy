a89afd2574e61323dabb17f53f6c962417ae74b7
Shraddha Kalburgi <shraddha.kalburgi@ad.infosys.com>
2025-04-22 21:42:33 +0530

    admin/products changes

--- nodeJSUdemyProject/controllers/admin.js
+++ nodeJSUdemyProject/controllers/admin.js
@@ -19,10 +19,10 @@ exports.postAddProducts = (req,res,next)=> {
 
 exports.getProducts = (req,res,next)=> {
     Product.fetchAll((products) => {
-        res.render('shop',{
+        res.render('admin/products',{
             prods:products,
-            pageTitle:'Shop',
-            path:'/'
+            pageTitle:'Admin Products',
+            path:'/admin/products'
         });
     });
 }
